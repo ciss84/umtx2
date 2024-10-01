fb00bca8d00e91934f9f074e1d48397580b4a25f
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-10-01 13:51:11 +0200

    Add files via upload

--- webkit.js
+++ webkit.js
@@ -391,6 +391,7 @@
     async function run_psfree(){
     document.getElementById("run-jb-parent").style.opacity = "0";
     document.getElementById("console-parent").style.opacity = "1";
+    showMessage("[+] running psfree for userland exploit..."),
     debug_log("[+] running psfree for userland exploit..."); 
     await get_ready();
     await use_after_free(pop, s1);
